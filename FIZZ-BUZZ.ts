5e6f0c6cd8a481d8285e05db874343e73a23d6ce
Jacqui23 <jacquelynedward98@gmail.com>
2023-04-14 11:48:33 +0200

    first commit

---
+++ FIZZ-BUZZ.ts
@@ -0,0 +1,13 @@
+
+function fizz(number : number) : any {
+    if (number % 3 == 0 && number % 5 == 0) return "FIZZ-BUZZ";
+    if (number % 3 == 0)  return "FIZZ";
+    if (number % 5 == 0)  return "BUZZ";
+
+    else return number
+
+        
+
+    }
+    
+   console.log (fizz(30))
